bec4394714b8a07ad416a4cfc750f88e77895a47
SatoShun <shun.sato1@gmail.com>
2019-10-15 02:32:35 +0000

    databinding

--- app/src/main/java/com/github/satoshun/example/AppActivity.kt
+++ app/src/main/java/com/github/satoshun/example/AppActivity.kt
@@ -4,14 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.github.satoshun.example.centerwithdivider.CenterWithDividerActivity
+import com.github.satoshun.example.databinding.AppActBinding
 
 class AppActivity : AppCompatActivity() {
+  private lateinit var binding: AppActBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.app_act)
+    binding = DataBindingUtil.setContentView(this, R.layout.app_act)
 
-    findViewById<View>(R.id.center_with_divider).setOnClickListener {
+    binding.centerWithDivider.setOnClickListener {
       startActivity(
         Intent(this@AppActivity, CenterWithDividerActivity::class.java)
       )
